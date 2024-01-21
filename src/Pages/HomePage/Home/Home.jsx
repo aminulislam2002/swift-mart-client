7b5cfb51d3d930fe1faf49724c09138de065f577
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-01-21 19:51:07 +0600

    design discover more section with swiper

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,9 +1,11 @@
+import DiscoverMore from "../HomePageSections/DiscoverMore/DiscoverMore";
 import TopBanner from "../HomePageSections/TopBanner/TopBanner";
 
 const Home = () => {
   return (
     <div>
       <TopBanner></TopBanner>
+      <DiscoverMore></DiscoverMore>
       <h1>Home</h1>
     </div>
   );
